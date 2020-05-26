cde832c3d57be27fe66ef57cf0c3793bdb482871
Jose Emiliano Celestino Nuñez <emilianowii@gmail.com>
2020-05-26 18:51:50 -0500

    Se agregaron los  archivos de texto y de python de CursoTema y se agrego la clase CursoTema

---
+++ curso_tema.py
@@ -0,0 +1,24 @@
+class CursoTema:
+    def __init__ (self, idCursoTema, idCurso, idTema):
+        self.__idCursoTema = idCursoTema
+        self.__idCurso = idCurso
+        self.__idTema = idTema
+    @property
+    def idCursoTema(self):
+        return self.__idCursoTema
+    @property
+    def idCurso(self):
+        return self.__idCurso
+    @property
+    def idTema(self):
+        return self.__idTema
+    @idCursoTema.setter
+    def idCursoTema(self, nuevoValor):
+        self.__idCursoTema = nuevoValor
+    @idCurso.setter
+    def idCurso(self, nuevoValor):
+        self.__idCurso = nuevoValor
+    @idTema.setter
+    def idTema(self, nuevoValor):
+        self.__idTema = nuevoValor
+            
