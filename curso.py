22fdc3aad595744b6c38ad37d40a95ed9b6ec989
RicardoBanda <alexjeje453@gmail.com>
2020-05-26 19:01:11 -0500

    Agregue cambios

--- curso.py
+++ curso.py
@@ -8,15 +8,33 @@ class Curso:
      def idcurso(self):
          return self.__idcurso
 
-    def infocurso(self):
-        return f"{self.__idcurso}|{self.__descripcion}|{self.__idempleado}"
+     def infocurso(self):
+         return f"{self.__idcurso}|{self.__descripcion}|{self.__idempleado}"
 
 print ("Menu")
 while True:
     print ("Este es el menu para el curso")
-    opcion=int(input("Opcion 1 Agregar, ¿Cual es la opcion que eliges?\n"))
-    if opcion==1
-    agregar= open
+    opcion=int(input("Opcion [1] Agregar\n, ¿Cual es la opcion que eliges?\n"))
+    if opcion==[1]:
+        agregar= open("./archivos/cursos.txt",'a')
+       
+    idcurso=int(input("¿Cual es el id del curso?\n"))
+    descripcion=input("¿Cual es la descripcion del curso?\n")
+    idempleado=int(input("¿Cual es el id del empleado?\n"))
+
+    curso=Curso(idcurso,descripcion,idempleado)
+    info=curso.infocurso()
+
+    agregar.write(info)
+
+    agregar.close()
+
+
+
+
+
+
+
 
 
 
