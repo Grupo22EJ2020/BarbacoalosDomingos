250ab09e313db4a8aa86c0924e8f887721688e46
Jose Emiliano Celestino Nuñez <emilianowii@gmail.com>
2020-05-26 20:31:40 -0500

    Se agregó la opción de Agregar CursoTema

--- curso_tema.py
+++ curso_tema.py
@@ -29,5 +29,32 @@ def validacionDeDato(cadena):
     except ValueError:
         return False
 
-print("Menú(TemaCurso)\n1.-Agregar Tema asignado a un curso.\n2.-Borrar Tema asignado a un curso\n3.-Modificar Tema asignado a un curso\n4.-Consultar todo\n5.-Consultar un Tema asignado a un curso en específico.")
-   
+def main():
+    print("Menú(TemaCurso)\n1.-Agregar Tema asignado a un curso.\n2.-Borrar Tema asignado a un curso\n3.-Modificar Tema asignado a un curso\n4.-Consultar todo\n5.-Consultar un Tema asignado a un curso en específico.")
+    menu = int(input("Qué desea hacer?:\n"))
+    if menu == 1:
+        idCursoTema = input("Coloque el idCursoTema:\n")
+        while validacionDeDato(idCursoTema) == False:
+            print("Valor ingresado no válido, intente de nuevo.")
+            idCursoTema = input("Coloque el idCursoTema:\n")
+
+        idCurso = input("Coloque el idCurso:\n")
+        while validacionDeDato(idCurso) == False:
+            print("Valor ingresado no válido, intente de nuevo.")
+            idCurso = input("Coloque el idCurso:\n")
+
+        idTema = input("Coloque el idTema:\n")
+        while validacionDeDato(idTema) == False:
+            print("Valor ingresado no válido, intente de nuevo.")
+            idTema = input("Coloque el idTema:\n")
+            
+        nuevo = CursoTema(idCursoTema, idCurso, idTema)
+        archivo = open("./archivos/curso_tema.txt","w")
+        archivo.write(f"{nuevo.idCursoTema}|{nuevo.idCurso}|{nuevo.idTema}")
+        archivo.close()
+        print("Menú(TemaCurso)\n1.-Agregar Tema asignado a un curso.\n2.-Borrar Tema asignado a un curso\n3.-Modificar Tema asignado a un curso\n4.-Consultar todo\n5.-Consultar un Tema asignado a un curso en específico.")
+        menu = int(input("Qué desea hacer?:\n"))
+main()
+
+    
+
