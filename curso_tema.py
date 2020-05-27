3dfe1ada672060478bd9dd6bd5732ca1364d8b45
Jose Emiliano Celestino Nuñez <emilianowii@gmail.com>
2020-05-26 21:58:06 -0500

    Se agregó la opción de Modificar el CursoTema

--- curso_tema.py
+++ curso_tema.py
@@ -54,7 +54,7 @@ def main():
             archivo.write(f"{nuevo.idCursoTema}|{nuevo.idCurso}|{nuevo.idTema}\n")
             archivo.close()
             print("El curso se ha agregado con éxito")
-            print("\nMenú(TemaCurso)\n1.-Agregar Tema asignado a un curso.\n2.-Borrar Tema asignado a un curso.\n3.-Modificar Tema asignado a un curso.\n4.-Consultar todo.\n5.-Consultar un Tema asignado a un curso en específico.6.-Salir.")
+            print("\nMenú(TemaCurso)\n1.-Agregar Tema asignado a un curso.\n2.-Borrar Tema asignado a un curso.\n3.-Modificar Tema asignado a un curso.\n4.-Consultar todo.\n5.-Consultar un Tema asignado a un curso en específico.\n6.-Salir.")
             menu = int(input("Qué desea hacer?:\n"))
         if menu == 2:
             list = []
@@ -73,8 +73,38 @@ def main():
             print("El curso se ha borrado con éxito")
             print("\nMenú(TemaCurso)\n1.-Agregar Tema asignado a un curso.\n2.-Borrar Tema asignado a un curso.\n3.-Modificar Tema asignado a un curso.\n4.-Consultar todo.\n5.-Consultar un Tema asignado a un curso en específico.\n6.-Salir.")
             menu = int(input("Qué desea hacer?:\n"))
+        if menu == 3:
+            list2 = []
+            datoviejo= input("Coloque el idCursoTema que desee modificar:\n")
+            while validacionDeDato(datoviejo) == False:
+                print("Valor ingresado no válido, intente de nuevo.")
+                datoviejo = input("Coloque el idCursoTema que desee modificar:\n")
+            datonuevo1 = input("Coloque el nuevo valor de idCursoTema:\n")
+            while validacionDeDato(datonuevo1) == False:
+                print("Valor ingresado no válido, intente de nuevo.")
+                datonuevo1 = input("Coloque el nuevo valor de idCursoTema:\n")
+            datonuevo2 = input("Coloque el el nuevo valor de idCurso:\n")
+            while validacionDeDato(datonuevo2) == False:
+                print("Valor ingresado no válido, intente de nuevo.")
+                datonuevo2 = input("Coloque el nuevo valor idCurso:\n")
+            datonuevo3 = input("Coloque el nuevo valor de idTema:\n")
+            while validacionDeDato(datonuevo3) == False:
+                print("Valor ingresado no válido, intente de nuevo.")
+                datonuevo3 = input("Coloque el nuevo valor de idTema:\n")
+            archivo = open("./archivos/curso_tema.txt","r")
+            for renglon in archivo:
+                if datoviejo in renglon:
+                    list2.append(f"{datonuevo1}|{datonuevo2}|{datonuevo3}\n")
+                else:
+                    list2.append(renglon)
+            archivo.close()
+            archivo = open("./archivos/curso_tema.txt","w")
+            for dato in list2:
+                archivo.write(dato)
+            archivo.close()
+            print("\nMenú(TemaCurso)\n1.-Agregar Tema asignado a un curso.\n2.-Borrar Tema asignado a un curso.\n3.-Modificar Tema asignado a un curso.\n4.-Consultar todo.\n5.-Consultar un Tema asignado a un curso en específico.\n6.-Salir.")
+            menu = int(input("Qué desea hacer?:\n"))
         if menu == 6:
             break
 main()
     
-
