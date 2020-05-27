91d0fa14a2e0f5ff05488d20423019fce3c3ea11
RicardoBanda <alexjeje453@gmail.com>
2020-05-27 14:02:24 -0500

    Agruegue la opcion 3

--- curso.py
+++ curso.py
@@ -11,14 +11,14 @@ class Curso:
      def infocurso(self):
          return f"{self.__idcurso}|{self.__descripcion}|{self.__idempleado}"
 lista_curso=[]
-print ("Menu")
+print (" Bienvenido al menu")
 while True:
     print ("Este es el menu para el curso")
-    opcion=int(input("Opcion [1] Agregar\n,Opcion [2] Eliminar\n,Opcion [3] Modificar\n,Opcion [4] Consultar\n,¿Cual es la opcion que eliges?\n"))
-    if opcion==[1]:
-        archivo= open("./archivos/cursos.txt",'a')
+    opcion=int(input(" Opcion [1] Agregar\n Opcion [2] Eliminar\n Opcion [3] Modificar\n Opcion [4] Consultar\n ¿Cual es la opcion que eliges?\n"))
+    if opcion==1:
+        archivo = open("./archivos/cursos.txt",'a')
        
-        idcurso=int(input("¿Cual es el id del curso?\n"))
+        idCurso=int(input("¿Cual es el id del curso?\n"))
         descripcion=input("¿Cual es la descripcion del curso?\n")
         idempleado=int(input("¿Cual es el id del empleado?\n"))
 
@@ -28,16 +28,34 @@ while True:
         archivo.write(info)
 
         archivo.close()
-    elif opcion==[2]:
-        if lista_curso==[]:
-            print("Ingrese el id del curso que desea borrar")
-        else:
-            reg=int(input("Registros:"))
-            for borrar in lista_curso:
-                if borrar.idcurso==reg:
-                    lista_curso.remove(Curso(reg,None,None))
-                    input("El curso a sido eliminado")
-                    archivo.close
+    
+    if opcion==2:
+        list ==[]
+        borrar=print("Ingrese el id del curso que desea borrar")
+        archivo = open("./archivos/cursos.txt",'a')
+        for renglon in archivo:
+            R = renglon.split("|")
+            if borrar == R[0]:
+                 pass
+            else:
+                list.append(renglon) 
+            archivo.close()
+            archivo = open("./archivos/cursos.txt",'a')
+            for curso in list:
+                 archivo.write(curso)
+            archivo.close()
+            print("El curso fue eliminado")
+   
+           
+    if opcion==3:
+        archivo=open("./archivos/cursos.txt",'a')
+        for renglon in archivo:
+            infocurso=renglon.split('|')
+            print(F'IdCurso: {infocurso[0]} Descripcion:{infocurso[0]} IdEmpleado: {infocurso[0]}')
+            archivo.close()
+            
+
+
 
 
 
